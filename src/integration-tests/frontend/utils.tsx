f2e568bdde9626a5a1380b9baa94a844fa458971
yuichiyasui <yassan.b.a.w@gmail.com>
2025-04-18 01:33:37 +0900

    Add msw-trpc integration with HTTP link and transformer support

--- src/integration-tests/frontend/utils.tsx
+++ src/integration-tests/frontend/utils.tsx
@@ -1,6 +1,39 @@
 import { render } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
+import { httpLink as TRPCClientHttpLink } from "@trpc/client";
+import { createTRPCNext } from "@trpc/next";
 import type { ReactNode } from "react";
+import SuperJSON from "superjson";
+import type { AppRouter } from "~/server/routers/_app";
+import { createTRPCMsw, httpLink } from "~/utils/msw-trpc";
+
+export const trpc = createTRPCNext<AppRouter>({
+  config: () => {
+    return {
+      links: [
+        TRPCClientHttpLink({
+          url: "http://localhost:3000/api/trpc",
+          headers() {
+            return {
+              "content-type": "application/json",
+            };
+          },
+          transformer: SuperJSON,
+        }),
+      ],
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+          },
+        },
+      },
+    };
+  },
+  transformer: SuperJSON,
+});
 
 export const customRender = (ui: ReactNode) => {
   const user = userEvent.setup();
@@ -10,3 +43,20 @@ export const customRender = (ui: ReactNode) => {
     ...render(ui),
   };
 };
+
+export const trpcMsw = createTRPCMsw<AppRouter>({
+  transformer: {
+    input: SuperJSON,
+    output: SuperJSON,
+  },
+  links: [
+    httpLink({
+      url: "http://localhost:3000/api/trpc",
+      headers() {
+        return {
+          "content-type": "application/json",
+        };
+      },
+    }),
+  ],
+});
